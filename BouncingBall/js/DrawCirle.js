0068a898b946f85166316d118087abb77ed027ed
pramod <pramod@tavisca.com>
2014-07-26 17:11:06 +0530

    Added Bouncing Ball and Rss Feed Assignment to the Repo

---
+++ BouncingBall/js/DrawCirle.js
@@ -0,0 +1,24 @@
+﻿// set context
+//initialize the diagram
+//fill color in circle
+//draw circle
+
+var boouncingBallContext;
+
+function setContext()
+{  
+   
+    boouncingBallContext = bouncingBallCanvas.getContext('2d');
+
+    setInterval(drawCen)
+
+    boouncingBallContext.beginPath();
+
+    boouncingBallContext.fillStyle = "#070719";
+
+    boouncingBallContext.arc(100, 100, 20, 0, Math.PI * 2, true);
+
+    boouncingBallContext.closePath();
+
+    boouncingBallContext.fill();
+}
